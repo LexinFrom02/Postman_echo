5745605ff478e02f9f53ce102b9698f5626a7025
Aleksey Ivantsov <Lexin1n3@gmail.com>
2021-03-01 21:37:22 +0500

    Test added

---
+++ src/test/java/ru/netology/rest/PostmanEchoAPITest.java
@@ -0,0 +1,29 @@
+package ru.netology.rest;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class PostmanEchoAPITest {
+    @Test
+    void shouldPost() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
